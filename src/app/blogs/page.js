e919952a7bfcd32dc3addacf4bee181bcf089c1a
tharuAnish <namoanishtharu@gmail.com>
2024-09-01 16:27:27 +0530

    b-GET done

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,5 +1,23 @@
 import BlogOverview from "@/components/blog-overview"
 
-export default function Blogs() {
-  return <BlogOverview />
+async function fetchListOfBlogs() {
+  try {
+    const response = await fetch("http://localhost:3000/api/get-blogs", {
+      method: "GET",
+      cache: "no-store",
+    })
+    const result = await response.json()
+
+    return result?.data
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+export default async function Blogs() {
+  const blogList = await fetchListOfBlogs()
+
+  console.log(blogList)
+
+  return <BlogOverview blogList={blogList} />
 }
--- src/components/blog-overview/index.js
+++ src/components/blog-overview/index.js
@@ -1,17 +1,34 @@
 "use client"
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import AddNewBlog from "../add-new-blog"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from "../ui/button"
+import { useRouter } from "next/navigation"
+import { Label } from "../ui/label"
 
 const initialBlogFormData = {
   title: "",
   description: "",
 }
 
-export default function BlogOverview() {
+export default function BlogOverview({ blogList }) {
   const [openBlogDialog, setOpenBlogDialog] = useState(false)
   const [loading, setLoading] = useState(false)
   const [blogFormData, setBlogFormData] = useState(initialBlogFormData)
+
+  const router = useRouter()
+
+  useEffect(() => {
+    router.refresh()
+  }, [])
 
   console.log(blogFormData)
 
@@ -27,6 +44,7 @@ export default function BlogOverview() {
         setBlogFormData(initialBlogFormData)
         setOpenBlogDialog(false)
         setLoading(false)
+        router.refresh()
       }
       console.log(result)
     } catch (error) {
@@ -37,7 +55,7 @@ export default function BlogOverview() {
   }
 
   return (
-    <main className=" min-h-screen justify-center items-center flex flex-col gap-10  p-24">
+    <main className=" min-h-screen  flex flex-col gap-10  p-24">
       <AddNewBlog
         loading={loading}
         setLoading={setLoading}
@@ -47,6 +65,30 @@ export default function BlogOverview() {
         setBlogFormData={setBlogFormData}
         handleSaveBlogData={handleSaveBlogData}
       />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-5">
+        {blogList && blogList.length > 0 ? (
+          blogList.map((blogItem) => (
+            <Card className="pt-5 bg-stone-50">
+              <CardContent className="flex flex-col justify-between gap-5 h-full">
+                <div>
+                  <CardTitle className="mb-4">{blogItem?.title}</CardTitle>
+                  <CardDescription>{blogItem?.description}</CardDescription>
+                </div>
+                <div className=" flex gap-5  items-center">
+                  <Button onClick={() => handleEdit(blogItem)}>Edit</Button>
+                  <Button onClick={() => handleDeleteBlogByID(blogItem._id)}>
+                    Delete
+                  </Button>
+                </div>
+              </CardContent>
+            </Card>
+          ))
+        ) : (
+          <Label className="text-3xl font-extrabold">
+            No Blog found! Please add one
+          </Label>
+        )}
+      </div>
     </main>
   )
 }
